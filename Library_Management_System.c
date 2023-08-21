dede593d17ca0f1d2d104b7916398ddd4d6c9575
unknown <kajalghule111@gmsail.com>
2023-08-21 15:12:28 +0530

    add books and display

--- Library_Management_System.c
+++ Library_Management_System.c
@@ -1,35 +1,55 @@
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
-// #include <string.h>
+#include <conio.h>
 
-#define MAX_USERNAME_SIZE 50
-#define MAX_PASSWORD_SIZE 10
+
+#define MAX_NAME_LENGTH 50
+#define MAX_PASSWORD_LENGTH 10
 #define MAX_USERTYPE_SIZE 10
+#define MAX_USERS 3
+#define MAX_BOOKS 3
+
 struct Date {
     int day;
     int month;
     int year;
 };
 
-struct User_Creaditial {
-    char userEmail;
-    char password[MAX_PASSWORD_SIZE];
-    
-};
+// struct User {
+//     int memberId;
+//     char memberName[MAX_USERNAME_SIZE];
+//     char email;
+//     char password[MAX_PASSWORD_SIZE];
+//     struct Date joinDate;
+//     int isPaidUser;
+//     float fineAmount;
+//     char userType[MAX_USERTYPE_SIZE]; // Owner/Librarian/Member
+// };
 
-struct User {
+typedef struct {
     int memberId;
-    char memberName[MAX_USERNAME_SIZE];
-    char email;
-    char password[MAX_PASSWORD_SIZE];
+    char username[MAX_NAME_LENGTH];
+    char email[15];
+    char password[MAX_PASSWORD_LENGTH];
     struct Date joinDate;
     int isPaidUser;
     float fineAmount;
     char userType[MAX_USERTYPE_SIZE]; // Owner/Librarian/Member
+} User;
+
+struct Book{
+    int bookId;
+    char bookName[100];
+    char authorName[50];
+    char subject[50];
+    float bookPrice;
+    int isbn;
 };
 
-struct User users[5];
+struct Library {
+    struct Book books[MAX_BOOKS];
+    int count;
+};
 
 void inputDate(struct Date *d) {
     printf("Enter day, month, and year: ");
@@ -89,35 +109,30 @@ int daysDiffer(struct Date *d1, struct Date *d2) {
     return daysDifference;
 }
 
-//function to Sign Up
+User users[MAX_USERS];
+int userCount = 0;
+
 int signUp() {
-    
-    struct User newUser;
-    newUser.memberId = 0;
-    printf("Enter user Email\n");
-    scanf("%s",&newUser.email);
-    for(int i=0; i<5; i++) {
-        // printf("inside for loop\n");
-        int result = strcmp(&newUser.email, &users[i].email);
-        if(result == 0) {
-            printf("User is already exit");
-            return 0;
-        }
-        
+    printf("inside sign-up function\n");
+    if (userCount >= MAX_USERS) {
+        printf("Maximum user limit reached.\n");
+        return 0;
     }
 
-    printf("Enter user Password\n");
-    scanf("%s",&newUser.password);
+    User newUser;
+    printf("Enter a username: ");
+    scanf("%s", &newUser.username);
 
-    printf("Enter user Name\n");
-    scanf("%s",&newUser.memberName);
-    
-    printf("Enter user isPaidUser \n");
-    scanf("%d",&newUser.isPaidUser);
-    
-    printf("Enter user amount\n");
-    scanf("%f",&newUser.fineAmount);
-    
+    for (int i = 0; i < userCount; i++) {
+        if (strcmp(newUser.username, users[i].username) == 0) {
+            printf("Username already exists.\n");
+            return 0;
+        }
+    }
+
+    printf("Enter a password: ");
+    scanf("%s", &newUser.password);
+
     int userTypeChoice;
 
     printf("Select the user type:\n");
@@ -127,7 +142,6 @@ int signUp() {
     printf("Enter your choice: ");
     scanf("%d", &userTypeChoice);
     
-
     switch (userTypeChoice) {
         case 1:
             strcpy(newUser.userType, "Member");
@@ -140,58 +154,183 @@ int signUp() {
             break;
         default:
             printf("Invalid user type selection.\n");
-            break;
+            return 0;
     }
-    printf("Sign up successfully !!\n");
+
+    users[userCount++] = newUser;
+    printf("*******************\n");
+    printf("Sign-up successful!\n");
+    printf("*******************\n");
     return 1;
 }
 
+
 void ownerMenu() {
-    printf("Inside owner menu");
+    // Implement owner menu
+    printf("Owner Menu\n");
+}
+
+
+void addNewBook(struct Library *library, const char *title) {
+    for (int i = 0; i < library->count; i++) {
+        if (strcmp(library->books[i].bookName, title) == 0) {
+            printf("Book already exists. Increasing count.\n");
+            return;
+        }
+    }
+    
+    if (library->count < MAX_BOOKS) {
+        strcpy(library->books[library->count].bookName, title);
+        printf("Enter Author Name: \n");
+        scanf("%s", &library->books[library->count].authorName);
+        printf("Enter Book subject: \n");
+        scanf("%s", &library->books[library->count].subject);
+        printf("Enter Book Price: \n");
+        scanf("%f", &library->books[library->count].bookPrice);
+        printf("Enter Book isbn: \n");
+        scanf("%d", &library->books[library->count].isbn);
+        library->count++;
+        printf("Book added successfully.\n");
+        printf("*******************\n");
+    } else {
+        printf("Library is full. Cannot add more books.\n");
+        printf("*******************\n");
+    }
+}
+
+
+void findBook(struct Library *library, const char *title) {
+    for (int i = 0; i < library->count; i++) {
+        if (strcmp(library->books[i].bookName, title) == 0) {
+            printf("Book found:\n");
+            printf("*********Books Details**********\n");
+            printf("Book Name: %s\n", library->books[i].bookName);
+            printf("Auther Name: %s\n", library->books[i].authorName);
+            printf("Book Subject: %s\n", library->books[i].subject);
+            printf("Book Price: %0.3f\n", library->books[i].bookPrice);
+            printf("Book isbn: %d\n", library->books[i].isbn);
+            printf("*******************\n");
+            return;
+        }
+    }
+    printf("Book not found.\n");
+}
+
+void displayAllBooks(struct Library *library) {
+    if (library->count == 0) {
+        printf("Library is empty.\n");
+        return;
+    }
+
+    printf("Books in the library:\n");
+    for (int i = 0; i < library->count; i++) {
+        printf("\n");
+        printf("Book %d:\n", i + 1);
+        printf("Book Name: %s\n", library->books[i].bookName);
+        printf("Author Name: %s\n", library->books[i].authorName);
+        printf("Book Price: %0.3f\n", library->books[i].bookPrice);
+        printf("Book Subject: %s\n", library->books[i].subject);
+        printf("Book isbn: %d\n", library->books[i].isbn);
+        printf("\n");
+    }
 }
 
 void librarianMenu() {
-    printf("####");
+    // Implement librarian menu
+    struct Library library;
+    library.count = 0;
+    char bookName[100];
+    int choice;
+    printf("*******************\n");
+    printf("Librarian Menu\n");
+    printf("*******************\n");
+    do {
+        printf("0. Exit\n");
+        printf("1. Add New Book\n");
+        printf("2. Find Book By Name\n");
+        printf("3. Find All Books \n");
+        printf("4. Issue Copy\n");
+        printf("Enter Your Choice : \n");
+        scanf("%d",&choice);
+        printf("*******************\n");
+        switch (choice)
+        {
+        case 0:
+            printf("Exiting...\n");
+            break;    
+        case 1:
+            printf("Enter Book Name\n");
+            scanf("%s",&bookName);
+            addNewBook(&library,bookName);
+            printf("Book count: %d\n", library.count);
+            // printf("%d",library.count);
+            break;
+        case 2:
+            printf("Enter Book Name to search: \n");
+            scanf("%s", &bookName);
+            findBook(&library, bookName);
+            break;
+        case 3:
+            displayAllBooks(&library);
+            break;
+        case 4:
+            /* code */
+            break;        
+        default:
+            break;
+        }
+    }while (choice != 0);
+    
+    
 }
 
 void memberMenu() {
-    printf("*****");
+    // Implement member menu
+    printf("Member Menu\n");
 }
 
-//function to sign in
+
+ 
 int signIn() {
-    char email;
-    char password[MAX_PASSWORD_SIZE];
+    char username[MAX_NAME_LENGTH];
+    char password[MAX_PASSWORD_LENGTH];
 
-    printf("Enter your email: ");
-    scanf("%s", &email);
+    printf("Enter your username: ");
+    scanf("%s", &username);
 
-    for (int i = 0; i < 5; i++) {
-        if (strcmp(email, users[i].email) == 0) {
+    for (int i = 0; i < userCount; i++) {
+        if (strcmp(username, users[i].username) == 0) {
             printf("Enter your password: ");
             scanf("%s", &password);
 
             if (strcmp(password, users[i].password) == 0) {
+                printf("*******************\n");
                 printf("Sign-in successful!\n");
-                if (strcmp(users[i].userType, "Owner") == 0) {
-                        ownerMenu();
-                } else if (strcmp(users[i].userType, "Librarian") == 0) {
-                        librarianMenu();
-                } else if (strcmp(users[i].userType, "Member") == 0) {
-                        memberMenu();
-                } else {
-                    printf("Invalid user type.\n");
-                }
-                return 1;
+                printf("*******************\n");
+                // return 1;
             } else {
                 printf("Incorrect password.\n");
                 return 0;
             }
         }
+        // Validate user and set user type
+    if (strcmp(users[i].userType, "Owner") == 0) {
+        ownerMenu();
+        return 1;
+    } else if (strcmp(users[i].userType, "Librarian") == 0) {
+        librarianMenu();
+        return 1;
+    } else if (strcmp(users[i].userType, "Member") == 0) {
+        memberMenu();
+        return 1;
+    } else {
+        printf("Invalid user type.\n");
     }
+    }
+
+    printf("Username not found.\n");
+    return 0;
 }
-
-
 
 
 int main () {
